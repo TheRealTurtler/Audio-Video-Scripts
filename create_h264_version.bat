af96248a7cbd80db519dbe8b9d26dc3d929694c3
TheRealTurtler <michael.diedler.7399@gmail.com>
2022-04-20 23:53:02 +0200

    Added a script to create h.264 versions of multiple files

---
+++ create_h264_version.bat
@@ -0,0 +1,29 @@
+:: Script to create a h.264 version of all input files
+:: Conversion is done using constant quality 18
+
+@echo off
+setlocal enabledelayedexpansion
+
+set /p input=Copy Subtitles? (y/n):
+if /I "%input%"=="y" goto with_subtitles
+goto without_subtitles
+
+:with_subtitles:
+for %%a in (%*) do (
+	for /F "delims=" %%i in (%%a) do (
+		set outfile=%%~di%%~pi%%~ni - H264%%~xi
+		ffmpeg -hide_banner -y -i %%a -c:a copy -c:s copy -c:v h264_amf -rc cqp -qp_i 18 -qp_p 18 -qp_b 18 "!outfile!"
+	)
+)
+goto exit
+
+:without_subtitles
+for %%a in (%*) do (
+	for /F "delims=" %%i in (%%a) do (
+		set outfile=%%~di%%~pi%%~ni - H264%%~xi
+		ffmpeg -hide_banner -y -i %%a -c:a copy -c:v h264_amf -rc cqp -qp_i 18 -qp_p 18 -qp_b 18 "!outfile!"
+	)
+)
+goto exit
+
+:exit
