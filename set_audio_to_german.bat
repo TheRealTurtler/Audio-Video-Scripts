3f1e169f9a88c219b41c0dccb66606e2960c16e5
TheRealTurtler <michael.diedler.7399@gmail.com>
2022-06-19 14:08:50 +0200

    set_audio_to_german script now correctly works with folders

--- set_audio_to_german.bat
+++ set_audio_to_german.bat
@@ -1,13 +1,42 @@
 @echo off
-setlocal enabledelayedexpansion
 
-for %%a in (%*) do (
-	for /F "delims=" %%i in (%%a) do (
-		set tempfile=%%~di%%~pitemp_%%~ni%%~xi
-		ffmpeg -hide_banner -y -i %%a -c copy -metadata:s:a language=deu "!tempfile!"
-		if !errorlevel! equ 0 (
-			del %%a
-			rename "!tempfile!" "%%~ni%%~xi"
-		)
-	)
+set input=%*
+
+:: Get input path(s) if needed.
+if not defined input set /p "input=Enter MKV path(s): "
+
+:: Exit if no input.
+if not defined input exit /b
+
+for %%A in (%input%) do call :step_1 "%%~A"
+exit /b
+
+:: Process a folder of MKV files or a single file.
+:step_1
+2>nul pushd "%~1" && (
+    for %%A in (*.mkv *.mp4) do call :step_2 "%%~A"
+    popd
+    goto exit_script
 )
+
+:: Exit if not MKV file.
+if /i not "%~x1" == ".mkv" exit /b 1
+
+:: Call single MKV file.
+call :step_2 "%~1"
+exit /b
+
+:step_2
+mkdir "%~d1%~p1..\temp"
+ffmpeg -hide_banner -y -i "%~f1" -c copy -metadata:s:a language=deu "%~d1%~p1..\temp\temp_%~n1%~x1"
+if %errorlevel% equ 0 move /y "%~d1%~p1..\temp\temp_%~n1%~x1" "%~f1"
+for /F %%i in ('dir /b "%~d1%~p1..\temp\*.*"') do (
+   echo Folder temp is not empty.
+   exit /b
+)
+call :delete_temp_folder "%~d1%~p1..\temp\"
+exit /b
+
+:delete_temp_folder
+del "%~f1"
+exit /b
