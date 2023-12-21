a2530010c75061ff74ee7d5b9ea365cce259c334
TheRealTurtler <michael.diedler.7399@gmail.com>
2023-12-21 13:59:25 +0100

    Adjusted error level to warning - This way even it is easier to spot corrupted parts of the file

--- check_video_corruption.bat
+++ check_video_corruption.bat
@@ -5,6 +5,6 @@ for %%a in (%*) do (
 	for /F "delims=" %%i in (%%a) do (
 		set logfile=%~dp0script_output\%%~ni.log
 		echo "!logfile!"
-		ffmpeg -v error -i %%a -f null - > "!logfile!" 2>&1
+		ffmpeg -v warning -i %%a -f null - > "!logfile!" 2>&1
 	)
 )
