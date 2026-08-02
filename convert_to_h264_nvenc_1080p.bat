dfb9fa15a85ad3b1afe25d8cc9fbcf63d949db77
TheRealTurtler <michael.diedler.7399@gmail.com>
2026-08-02 14:11:52 +0200

    Added NVENC versions for h264 and h265

---
+++ convert_to_h264_nvenc_1080p.bat
@@ -0,0 +1,45 @@
+@echo off
+
+rem ============================================================================
+rem  DESCRIPTION
+rem ============================================================================
+rem  Wrapper script for running convert_to_xyz.bat with fixed encoder settings.
+rem
+rem  ab-av1 is used to determine the optimal CRF based on VMAF analysis.
+rem  After the best CRF is found, all input files are encoded to H.264 (h264_nvenc).
+rem
+rem  NOTE: Only supports 8-bit encoding.
+rem  NOTE: File sizes will be larger than with x264!
+rem ============================================================================
+
+
+rem Encoder settings
+set ENCODER=h264_nvenc
+set PRESET=slow
+
+rem Additional settings for encoding (analysis and final encode)
+set SETTINGS_ENCODE_ALWAYS=--enc spatial-aq=1 --enc temporal-aq=1 --enc aq-strength=8 --enc rc-lookahead=32 --enc rc=vbr --pix-format yuv420p --vfilter scale=1920:-2 --enc profile:v=high --enc level:v=4.0
+
+rem Additional settings for analysis only
+set SETTINGS_ENCODE_ANALYSIS=--max-encoded-percent=1000
+
+rem Additional settings for final encoding only
+set SETTINGS_ENCODE_FINAL=--acodec libfdk_aac
+
+set OUTPUT_DIR=h264
+set ERROR_LOG=h264-failed.txt
+
+rem Number of threads to use (-1 = all)
+set THREADS=8
+
+
+call "%~dp0convert_to_xyz.bat" ^
+    %ENCODER% ^
+    %PRESET% ^
+	"%SETTINGS_ENCODE_ALWAYS%" ^
+    "%SETTINGS_ENCODE_ANALYSIS%" ^
+    "%SETTINGS_ENCODE_FINAL%" ^
+    %OUTPUT_DIR% ^
+    %ERROR_LOG% ^
+    %THREADS% ^
+    %*
